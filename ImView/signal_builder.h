060b2b9271888e523c2292d9cd833598becca50f
ivanos121 <loggie121@gmail.com>
2025-09-08 14:07:41 +0300

    two ventparam structures9

--- ImView/signal_builder.h
+++ ImView/signal_builder.h
@@ -1,6 +1,10 @@
 #ifndef SIGNAL_BUILDER_H
 #define SIGNAL_BUILDER_H
 
+#include "qchartview.h"
+#include "qlineseries.h"
+#include "qvalueaxis.h"
+#include "create_signal_builder.h"
 #include <QWidget>
 
 namespace Ui {
@@ -18,8 +22,37 @@ public:
 protected slots:
     void apply_signal_builder();
     void close_signal_builder();
-private:
+
+
+public:
     Ui::Signal_builder *ui;
+    QChartView *chartView;
+    QLineSeries *series;
+    QValueAxis *axisX;
+    QValueAxis *axisY;
+
+    void saveDataXml();
+    void loadDataXml();
+    void updateAxes();
+
+signals:
+    void dataReady(const QVector<QPointF> &points);
+
+protected slots:
+    bool savePointsToXml(const QString &fileName);
+    bool loadPointsFromXml(const QString &fileName);
+    void resieve_dannie();
+private slots:
+    void on_pushButtonAdd_clicked();
+    void on_pushButtonClear_clicked();
+
+private:
+    void updateChart();
+    Create_signal_builder signalBuilder;
+    QVector<QPointF> paintdata;
+    QChart *chart;
+    QString fileName;
+    //Graph *graph;
 };
 
 #endif // SIGNAL_BUILDER_H
